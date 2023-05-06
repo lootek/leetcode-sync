fed64b4dd23cde6820662c61dc4727967d59f2fc
Piotr Lucinski <lootek@users.noreply.github.com>
2023-05-06 11:34:48 -0700

    restore test_config.js

---
+++ src/test_config.js
@@ -0,0 +1,10 @@
+// Modify this file to run index.js locally and not as a GitHub Action.
+
+module.exports = {
+  GITHUB_TOKEN: null,
+  // Form of "<owner>/<repo_name>"
+  GITHUB_REPO: null,
+  
+  LEETCODE_CSRF_TOKEN: null,
+  LEETCODE_SESSION: null
+}
